12ee193854ccb1c111bf53b2d1a5db479cbab9ac
William Adams <spam@nanotechstyles.com>
2016-12-22 05:10:51 -0800

    Adding randomtriangles example

---
+++ examples/randomtriangles/randomtriangles.cpp
@@ -0,0 +1,67 @@
+// randomtriangles.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "drawproc.h"
+
+bool outlineOnly = true;
+int maxsize = 64;
+
+void keyTyped()
+{
+	switch (keyChar)
+	{
+	case ' ':
+		outlineOnly = !outlineOnly;
+		break;
+
+	}
+}
+
+void draw()
+{
+	background(pWhite);
+
+	if (outlineOnly)
+		noFill();
+
+	for (int cnt = 5001; cnt; cnt--)
+	{
+		uint8_t r = random(255);
+		uint8_t g = random(255);
+		uint8_t b = random(255);
+
+		int x1 = random(maxsize, width - 1 - (maxsize*2));
+		int y1 = random(maxsize, height - 1 - (maxsize*2));
+
+
+		int minx = x1 - maxsize;
+		int miny = y1 - maxsize;
+
+		int xrange = (x1 + maxsize) - (x1 - maxsize);
+		int yrange = (y1 + maxsize) - (y1 - maxsize);
+
+		int x2 = (rand() % xrange) + minx;
+		int y2 = (rand() % yrange) + miny;
+
+		int x3 = (rand() % xrange) + minx;
+		int y3 = (rand() % yrange) + miny;
+
+		if (!outlineOnly)
+		{
+			fill(r, g, b, 255);
+			noStroke();
+		}
+		else {
+			noFill();
+			stroke(r, g, b, 255);
+		}
+
+		triangle(x1, y1, x2, y2, x3, y3);
+	}
+}
+
+void setup()
+{
+	createCanvas(1024, 768);
+	background(pWhite);
+}
